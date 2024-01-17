5b9123f8efc73da75d116861e423a1cac01efa1a
HarleyMax <wakemanharley@yahoo.com>
2024-01-16 22:21:46 -0500

    Initialized React frontend and created header and footer

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import './app.css'
+
+export default function App() {
+    return (
+      <>
+        <Header/>
+        <Footer/> 
+      </>
+    );
+}
